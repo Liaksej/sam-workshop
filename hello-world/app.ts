875cc82bb9745e6fde528061ea3e23f586bbd01a
Liaksej <tarusov.liaksej@gmail.com>
2024-09-21 12:42:54 +0200

    Add conditional Canary deployment and CloudWatch alarm
    Comment out the lambda handler and unit tests, and enhance deployment settings to use a conditional Canary deployment strategy based on the stack name, including a CloudWatch alarm for monitoring errors in production.

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -11,20 +11,20 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  */
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "I'm using canary deployments",
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
-};
+//     try {
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify({
+//                 message: "I'm using canary deployments",
+//             }),
+//         };
+//     } catch (err) {
+//         console.log(err);
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({
+//                 message: 'some error happened',
+//             }),
+//         };
+//     }
+// };
